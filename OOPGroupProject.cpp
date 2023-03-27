8e6f5b93a4c8543e04aa597fbd2794839f7a5c41
KareemIssa <99224686+KareemIssa@users.noreply.github.com>
2023-03-27 17:27:17 -0400

    main

--- OOPGroupProject.cpp
+++ OOPGroupProject.cpp
@@ -10,6 +10,10 @@ ID: 400441896
 
 using namespace std;
 
+void studentMenu();
+void librarianMenu();
+
+
 int main()
 {
 	BookInfo rec[16];
@@ -35,14 +39,107 @@ int main()
 
 
 
+	char userType;
+
+	cout << "Are you a student or a librarian? (S/L): ";
+	cin >> UserType;
+	cout << endl;
+
+	if (userType == 'S' || userType == 's') {
+			studentMenu();
+	} else if (userType == 'L' || userType == 'l') {
+			librarianMenu();
+	} else {
+			cout << "Invalid option, please try again." << endl;
+	}
+
+
+
+
+
+
+
 	return 0;
 }
 
+void studentMenu() {
+    char userChoice;
+    bool programRunning = true;
 
-void Greetings(){
-	cout << "Hello, welcome JAKI Towns Public Library.\n";
-	cout << "Are you a Librarian(1) or Student(2)?";
-	int login;
-	cin >> login;
+    while (programRunning) {
+        cout << "************************************************************" << endl;
+        cout << "Welcome to JAKIs Library Management System!" << endl << endl;
+        cout << "Please select an option from the following menu:" << endl << endl;
+        cout << "1. Search for a book" << endl;
+        cout << "2. Borrow a book" << endl;
+        cout << "3. Return a book" << endl;
+        cout << "4. Exit" << endl << endl;
+        cout << "Please enter your choice: ";
 
+        cin >> user_choice;
+        cout << endl;
+
+        switch (userChoice) {
+            case '1':
+                //search for a book
+                break;
+            case '2':
+                //borrow book
+                break;
+            case '3':
+                //return a book
+                break;
+            case '4':
+                programRunning = false;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    }
+}
+
+void librarianMenu() {
+    char userChoice;
+    bool programRunning = true;
+
+    while (programRunning) {
+        cout << "************************************************************" << endl;
+        cout << "Welcome to JAKIs Library Management System!" << endl << endl;
+        cout << "Please select an option from the following menu:" << endl << endl;
+        cout << "1. Search for a book" << endl;
+        cout << "2. Add a new book" << endl;
+        cout << "3. Remove a book" << endl;
+        cout << "4. View all books" << endl;
+        cout << "5. View borrowed books" << endl;
+        cout << "6. Exit" << endl << endl;
+        cout << "Please enter your choice: ";
+
+        cin >> userChoice;
+        cout << endl;
+
+        switch (userChoice) {
+            case '1':
+                //search for a book
+                break;
+            case '2':
+                //add a new book
+                break;
+            case '3':
+                //remove a book
+                break;
+            case '4':
+                //view all books
+                break;
+            case '5':
+                //view borrowed books
+                break;
+            case '6':
+                programRunning = false;
+                break;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+                break;
+        }
+    }
 }
