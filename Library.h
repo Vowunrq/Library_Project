e25d99403a157c686e7679d88513d6e3d9f732d4
IvanCai335 <85745947+IvanCai335@users.noreply.github.com>
2023-03-27 18:42:45 -0400

    adding library removing bookinfo

---
+++ Library.h
@@ -0,0 +1,42 @@
+#include "Book.h"
+#include <fstream>
+
+Library::Library()
+{
+    title = "";
+    author = "";
+    genre = "";
+    year = 0;
+    availability = true;
+}
+
+Library::Library(string t, string auth, string g, int y, bool avail)
+{
+    title = t;
+    author = auth;
+    genre = g;
+    year = y;
+    availability = avail;
+}
+
+Library::~Library()
+{
+}
+
+void Library::BookCatalogue()
+{
+}
+
+void Library::DisplayBooks(Library object) {
+    ifstream bookfile;
+    while (!bookfile.eof()) {
+        cout << object.getTitle() << "_\t" << object.getAuthor();
+        cout << object.getGenre() << "_\t" << object.getYear();
+        cout << "_\t" << object.getAvailability();
+    }
+}
+
+ostream& operator<<(ostream& stream, Library object) {
+    stream << object.getTitle() << "_\t" << object.getAuthor();
+    stream << object.getGenre() << "_\t" << object.getYear();
+}
